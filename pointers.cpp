f5f22e9365bfdb69a35b685264a9aec91a3a90ca
Imyashmathur <yashkumarrahi@gmail.com>
2024-01-07 08:44:18 +0530

    n

---
+++ pointers.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num = 5;
+    cout << num <<endl;
+    cout<<"address of num is "<< &num <<endl;
+    int *ptr = &num;
+    cout<<"value is: "<<  ptr<<endl;
+    cout<<"value is: "<<*ptr<<endl;
+    cout<<"size of integer is "<<sizeof(num)<<endl;
+    cout<<"size of pointer is "<<sizeof(ptr)<<endl;
+    return 0;
+}
