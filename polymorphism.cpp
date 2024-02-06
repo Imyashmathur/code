dc5cb4ee2514409bc2177df318903572c267ddf6
Imyashmathur <yashkumarrahi@gmail.com>
2024-02-07 00:37:03 +0530

    new

---
+++ polymorphism.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+class A{
+public:
+void sayHello(){
+    cout<<"Hello Love Babbar"<<endl;
+}
+void sayHello(string name){
+    cout<<"Hello "<<name<<endl;
+}
+};
+
+int main(){
+    A  obj;
+    obj.sayHello();
+
+    return 0;
+}
---
+++ single.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+class Animal{
+    public:
+    int age;
+    int weight;
+
+    public:
+    void speak(){
+        cout<<"Speaking "<<endl;
+
+    }
+};
+class Dog:public Animal{
+
+};
+
+class GermanShepherd:public  Dog{
+
+};
+
+int main(){
+
+    Dog d;
+    d.speak();
+    cout<<d.age<<endl;
+
+    GermanShepherd g;
+    g.speak();
+
+    return 0;
+}
